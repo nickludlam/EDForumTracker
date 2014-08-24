bfbcaaf4b73480d085c7ba45bfd49a08476792dd
Nick Ludlam <nick@recoil.org>
2014-08-24 13:13:26 +0100

    More quips

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,15 +6,21 @@ class PostsController < ApplicationController
            'Busy day, I see.', 'No, nothing\'s changed!', 'There\'s always old episodes of Lave Radio!',
            'Nothing to seem, move along.', 'This isn\'t the page you\'re looking for, move along.',
            'Try reddit.com/r/EliteDangerous/ instead.', '100 CR fine for loitering near this tracker!',
-           'Warning! Flight Assist off!', 'Mass locked!' ]
+           'Warning! Flight Assist off!', 'Mass locked!', 'Clearly no-one\'s said \'Yaw is nerfed\' today.',
+           'The mugs must be flying in Cambridge.', 'They must all be fishing on Chango.',
+           'Obviously there\'s not enough discussion about \'griefing\' today.',
+           'There\'s no such thing as too much play-testing!' ]
+  
   def index
+    @authors = Author.all
+    @quip = QUIPS.sample
+
     if session[:last_post_id]
       @posts = Post.where("forum_post_id > ?", session[:last_post_id]).order("forum_post_id DESC")
     else
       @posts = Post.all.order("forum_post_id DESC").limit(50)
     end
-    @authors = Author.all
-    @quips = QUIPS
+
     session[:last_post_id] = @posts.first.forum_post_id unless @posts.empty?
   end
   
