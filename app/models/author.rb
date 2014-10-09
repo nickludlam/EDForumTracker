10ed2e0ae832dc734d5c73d2aa34ea12144f72ca
Nick Ludlam <nick@recoil.org>
2014-10-09 18:23:19 +0100

    Add delete_all dependency for Authors on their Posts, as well as a validation on uniqueness of forum_id

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,8 @@
 require 'open-uri'
 
 class Author < ActiveRecord::Base
-  has_many :posts
+  has_many :posts, dependent: :delete_all
+  validates_uniqueness_of :forum_id
   
   AUTHOR_STATS_URL_PREFIX = "http://forums.frontier.co.uk/member.php?u=%d"
   AUTHOR_POSTS_URL_PREFIX = "http://forums.frontier.co.uk/search.php?do=finduser&u=%d"
@@ -12,7 +13,6 @@ class Author < ActiveRecord::Base
     post_count_label = author_doc.at('span:contains("Total Posts")')
     if post_count_label
       current_post_count = post_count_label.parent.children.last.text.to_i
-      puts "Post count for author id #{forum_id} is #{current_post_count}"
       return current_post_count 
     else
       logger.warn("Failed to find the post count span for author id #{forum_id}")
@@ -21,7 +21,7 @@ class Author < ActiveRecord::Base
   end
   
   def cache_new_posts(replace_existing=false)
-    Rails.logger.debug("Starting cache_new_posts")
+    logger.debug("Starting cache_new_posts")
     posts_doc = Nokogiri::HTML(open(Author::AUTHOR_POSTS_URL_PREFIX % forum_id))
     threads_list_element = posts_doc.css("table#threadslist")[0]
     if threads_list_element
@@ -46,7 +46,7 @@ class Author < ActiveRecord::Base
       end
     end
     
-    Rails.logger.debug("Finished cache_new_posts")
+    logger.debug("Finished cache_new_posts")
   end
   
   def create_normalized_post(post_id, body, find_existing=false)
